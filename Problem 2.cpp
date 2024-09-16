1def1840ce78b3ad3daf3471109c0ee41ae911bb
aryajason <aryajasonr@gmail.com>
2024-09-16 18:47:14 +0700

    Problem 2 New.cpp

--- Problem 2.cpp
+++ Problem 2.cpp
@@ -1,31 +1,34 @@
-#include <iostream>
-#include <cmath>
+#include <iostream> // For Input and Output Function
+#include <cmath> // provides access to mathematical functions like sqrt()
 using namespace std;
 
 int main() 
 {
     
- float a,b,c;
+ float a,b,c; // Declaration Variable and User Input 
+ cout << "Enter The a, b, and c: ";
  cin>>a>>b>>c;
  
- float D= b*b-4*a*c;
+ float D= b*b-4*a*c; // Discrimination Calculation
  
- if (D<0) 
+ // Checking The Discrimination Calculation
+ if (D<0) // Case If D<0
  {
      cout<<" There are no real roots"<<endl;
  } 
- else if (D==0)
+ else if (D==0) // Case if D=0
  {
-   float x = (-b)/2*a;
+   float x = (-b)/2*a; // Calculation of Case of One Real Roots
    cout<<"x : "<<x<<endl;
  
- } else 
+ } else // Case If D>0
  {
-   float x1= ((b*-1)+ sqrt(D))/2*a;
-   float x2= ((b*-1)- sqrt(D))/2*a;
+   float x1= ((b*-1)+ sqrt(D))/(2*a); // Calculation of Case of Two Real Roots
+   float x2= ((b*-1)- sqrt(D))/(2*a);
    
    cout<<"x1 ="<<x1<<endl;
    cout<<"x2 ="<<x2<<endl;
    
+   return 0; // End Program
  }
 }
